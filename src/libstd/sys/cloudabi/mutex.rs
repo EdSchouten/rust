5fb05d0aafe8d3d7c692572055ce74f8e537cfc0
Ed Schouten <ed@nuxi.nl>
2017-12-26 11:51:28 +0100

    Put stubs in place for mutex and rwlock.

--- src/libstd/sys/cloudabi/mutex.rs
+++ src/libstd/sys/cloudabi/mutex.rs
@@ -1,3 +1,6 @@
+extern crate cloudabi;
+
+use mem;
 use sync::atomic::AtomicU32;
 
 pub unsafe fn raw(m: &Mutex) -> &mut AtomicU32 {
@@ -8,7 +11,61 @@ pub struct Mutex {
     lock: AtomicU32,
 }
 
+impl Mutex {
+    pub const fn new() -> Mutex {
+        Mutex { lock: AtomicU32::new(cloudabi::LOCK_UNLOCKED.0) }
+    }
+
+    pub unsafe fn init(&mut self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn try_lock(&self) -> bool {
+        // TODO(ed): Implement!
+        false
+    }
+
+    pub unsafe fn lock(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn unlock(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn destroy(&self) {
+        // TODO(ed): Implement!
+    }
+}
+
 pub struct ReentrantMutex {
     lock: AtomicU32,
     recursion: u32,
+}
+
+impl ReentrantMutex {
+    pub unsafe fn uninitialized() -> ReentrantMutex {
+        ReentrantMutex { ..mem::uninitialized() }
+    }
+
+    pub unsafe fn init(&mut self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn try_lock(&self) -> bool {
+        // TODO(ed): Implement!
+        false
+    }
+
+    pub unsafe fn lock(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn unlock(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn destroy(&self) {
+        // TODO(ed): Implement!
+    }
 }
--- src/libstd/sys/cloudabi/rwlock.rs
+++ src/libstd/sys/cloudabi/rwlock.rs
@@ -1,1 +1,43 @@
-pub struct RWLock {}
+extern crate cloudabi;
+
+use sync::atomic::AtomicU32;
+
+pub struct RWLock {
+    lock: AtomicU32,
+}
+
+impl RWLock {
+    pub const fn new() -> RWLock {
+        RWLock { lock: AtomicU32::new(cloudabi::LOCK_UNLOCKED.0) }
+    }
+
+    pub unsafe fn try_read(&self) -> bool {
+        // TODO(ed): Implement!
+        false
+    }
+
+    pub unsafe fn read(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn read_unlock(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn try_write(&self) -> bool {
+        // TODO(ed): Implement!
+        false
+    }
+
+    pub unsafe fn write(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn write_unlock(&self) {
+        // TODO(ed): Implement!
+    }
+
+    pub unsafe fn destroy(&self) {
+        // TODO(ed): Implement!
+    }
+}
