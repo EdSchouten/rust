87db5b934cc1b248ae91734bfec428b09f3d6306
Ed Schouten <ed@nuxi.nl>
2017-12-27 21:59:48 +0100

    Implement waiting on condition variables?

--- src/libstd/sys/cloudabi/condvar.rs
+++ src/libstd/sys/cloudabi/condvar.rs
@@ -1,6 +1,17 @@
+// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 extern crate cloudabi;
 
 use cell::UnsafeCell;
+use mem;
 use sync::atomic::{AtomicU32, Ordering};
 use sys::mutex::{self, Mutex};
 use time::Duration;
@@ -53,25 +64,30 @@ impl Condvar {
     pub unsafe fn wait(&self, mutex: &Mutex) {
         let mutex = mutex::raw(mutex);
         assert_eq!(
-            (*mutex).load(Ordering::Relaxed) & cloudabi::LOCK_KERNEL_MANAGED.0,
+            (*mutex).load(Ordering::Relaxed) & ~cloudabi::LOCK_KERNEL_MANAGED.0,
             __pthread_thread_id.0 | cloudabi::LOCK_WRLOCKED.0
         );
 
-        // TODO(ed): Implement!
-        /*
+        // Call into the kernel to wait on the condition variable.
+        let condvar = self.condvar.get();
         let subscription = cloudabi::subscription {
             type_: cloudabi::eventtype::CONDVAR,
             union: cloudabi::subscription_union {
                 condvar: cloudabi::subscription_condvar {
-                    condvar: &mut self.condvar as *mut _ as *mut cloudabi::condvar,
+                    condvar: condvar as *mut cloudabi::condvar,
                     condvar_scope: cloudabi::scope::PRIVATE,
-                    lock: mutex::raw(mutex) as *mut _ as *mut cloudabi::lock,
+                    lock: mutex as *mut cloudabi::lock,
                     lock_scope: cloudabi::scope::PRIVATE,
-                }
+                },
             },
             ..mem::zeroed()
         };
-        */    }
+        let mut event: cloudabi::event = mem::uninitialized();
+        let mut nevents: usize = mem::uninitialized();
+        let ret = cloudabi::poll(&subscription, &mut event, 1, &mut nevents);
+        assert_eq!(ret, cloudabi::errno::SUCCESS);
+        assert_eq!(event.error, cloudabi::errno::SUCCESS);
+    }
 
     pub unsafe fn wait_timeout(&self, _: &Mutex, _: Duration) -> bool {
         // TODO(ed): Implement!
