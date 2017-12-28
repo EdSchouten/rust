cd82f0d053d990c922d538e6b1962531175a7cee
Ed Schouten <ed@nuxi.nl>
2017-12-28 14:08:03 +0100

    Order by CloudABI definition.

--- src/libstd/sys/cloudabi/mod.rs
+++ src/libstd/sys/cloudabi/mod.rs
@@ -21,22 +21,21 @@ pub fn init() {}
 
 pub fn decode_error_kind(errno: i32) -> io::ErrorKind {
     match errno {
+        x if x == cloudabi::errno::ACCES as i32 => io::ErrorKind::PermissionDenied,
+        x if x == cloudabi::errno::ADDRINUSE as i32 => io::ErrorKind::AddrInUse,
+        x if x == cloudabi::errno::ADDRNOTAVAIL as i32 => io::ErrorKind::AddrNotAvailable,
         x if x == cloudabi::errno::AGAIN as i32 => io::ErrorKind::WouldBlock,
+        x if x == cloudabi::errno::CONNABORTED as i32 => io::ErrorKind::ConnectionAborted,
         x if x == cloudabi::errno::CONNREFUSED as i32 => io::ErrorKind::ConnectionRefused,
         x if x == cloudabi::errno::CONNRESET as i32 => io::ErrorKind::ConnectionReset,
-        x if x == cloudabi::errno::PERM as i32 || x == cloudabi::errno::ACCES as i32 => {
-            io::ErrorKind::PermissionDenied
-        }
-        x if x == cloudabi::errno::PIPE as i32 => io::ErrorKind::BrokenPipe,
-        x if x == cloudabi::errno::NOTCONN as i32 => io::ErrorKind::NotConnected,
-        x if x == cloudabi::errno::CONNABORTED as i32 => io::ErrorKind::ConnectionAborted,
-        x if x == cloudabi::errno::ADDRNOTAVAIL as i32 => io::ErrorKind::AddrNotAvailable,
-        x if x == cloudabi::errno::ADDRINUSE as i32 => io::ErrorKind::AddrInUse,
-        x if x == cloudabi::errno::NOENT as i32 => io::ErrorKind::NotFound,
+        x if x == cloudabi::errno::EXIST as i32 => io::ErrorKind::AlreadyExists,
         x if x == cloudabi::errno::INTR as i32 => io::ErrorKind::Interrupted,
         x if x == cloudabi::errno::INVAL as i32 => io::ErrorKind::InvalidInput,
+        x if x == cloudabi::errno::NOENT as i32 => io::ErrorKind::NotFound,
+        x if x == cloudabi::errno::NOTCONN as i32 => io::ErrorKind::NotConnected,
+        x if x == cloudabi::errno::PERM as i32 => io::ErrorKind::PermissionDenied,
+        x if x == cloudabi::errno::PIPE as i32 => io::ErrorKind::BrokenPipe,
         x if x == cloudabi::errno::TIMEDOUT as i32 => io::ErrorKind::TimedOut,
-        x if x == cloudabi::errno::EXIST as i32 => io::ErrorKind::AlreadyExists,
         _ => io::ErrorKind::Other,
     }
 }
