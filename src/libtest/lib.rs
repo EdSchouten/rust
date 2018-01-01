de8d32e1caeec7ca15499660baad7dd903b06199
Ed Schouten <ed@nuxi.nl>
2018-01-01 19:52:06 +0100

    Clean up the isatty() bits.

--- src/libtest/lib.rs
+++ src/libtest/lib.rs
@@ -953,12 +953,9 @@ fn use_color(opts: &TestOpts) -> bool {
     }
 }
 
-#[cfg(any(target_os = "cloudabi", target_os = "redox"))]
-fn stdout_isatty() -> bool {
-    // FIXME: Implement isatty.
-    false
-}
-#[cfg(all(target_arch = "wasm32", not(target_os = "emscripten")))]
+#[cfg(any(target_os = "cloudabi",
+          target_os = "redox",
+          all(target_arch = "wasm32", not(target_os = "emscripten"))))]
 fn stdout_isatty() -> bool {
     false
 }
