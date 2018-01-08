d4e1be0681168ccda734e850301f27aedbda0134
Ed Schouten <ed@nuxi.nl>
2018-01-08 23:14:13 +0100

    Use automatically inferred compiler knowledge.
    This now works with cc-rs 1.0.4.

--- src/ci/docker/scripts/cloudabi-toolchain.sh
+++ src/ci/docker/scripts/cloudabi-toolchain.sh
@@ -24,11 +24,6 @@ ln -s ../lib/llvm-5.0/bin/clang /usr/bin/${target}-c++
 ln -s ../lib/llvm-5.0/bin/lld /usr/bin/${target}-ld
 ln -s ../../${target} /usr/lib/llvm-5.0/${target}
 
-# TODO(ed): Extra symlinks for -clang and -clang++ to work around
-# https://github.com/alexcrichton/cc-rs/pull/278
-ln -s ../lib/llvm-5.0/bin/clang /usr/bin/${target}-clang
-ln -s ../lib/llvm-5.0/bin/clang /usr/bin/${target}-clang++
-
 # Install the C++ runtime libraries from CloudABI Ports.
 echo deb https://nuxi.nl/distfiles/cloudabi-ports/debian/ cloudabi cloudabi > /etc/apt/sources.list.d/cloudabi.list
 curl 'https://pgp.mit.edu/pks/lookup?op=get&search=0x0DA51B8531344B15' | apt-key add -
