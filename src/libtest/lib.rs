a823e5b477d05d0089aea6bbc0b21ed964c48a6c
Ed Schouten <ed@nuxi.nl>
2018-01-11 11:40:41 +0100

    Revert "Remove a change that might be unnecessary?"
    This reverts commit 5691d1c977e4aa75f2350f01169874ace33d7dfa.

--- src/libtest/lib.rs
+++ src/libtest/lib.rs
@@ -36,7 +36,7 @@
 
 #![feature(asm)]
 #![feature(fnbox)]
-#![cfg_attr(unix, feature(libc))]
+#![cfg_attr(any(unix, target_os = "cloudabi"), feature(libc))]
 #![feature(set_stdio)]
 #![feature(panic_unwind)]
 #![feature(staged_api)]
